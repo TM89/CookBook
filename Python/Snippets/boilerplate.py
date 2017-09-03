b691fd86ee72bd3cc3a1b2fb630f2fde08ac17d3
Torbjørn Mevik <torbjorn.mevik@bouvet.no>
2017-09-03 20:51:42 +0200

    Added snippet for standard boilerplate

---
+++ Python/Snippets/boilerplate.py
@@ -0,0 +1,12 @@
+#python boilerplate
+
+import sys
+
+def a_function(with_parameter):
+	#that does stuff here
+	print(with_parameters)
+	
+	
+if __name__ == "__main__":
+	#this is where it all begins
+	a_function("parameter_value")
